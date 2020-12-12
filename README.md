6bfb27ab61cad721bf5eb61baf4ceec002b33e1e
Mike Blijd <mikeblijd@gmail.com>
2020-12-12 03:44:39 +0100

    🚀Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # dark-mode
+## Dependencies
+vscode live sass compiler
 
 ## Project setup
 ```
@@ -22,3 +24,40 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+
+### Check for broswer support
+
+```
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<div id="result"></div>
+
+<script>
+// Check browser support
+if (typeof(Storage) !== "undefined") {
+    // Store
+    localStorage.setItem("list", "<h1>John<h1>");
+    appendToStorage("list", "<h2>David<h2>");
+
+    // Retrieve
+    document.getElementById("result").innerHTML = localStorage.getItem("list");
+} else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+}
+
+function appendToStorage(name, data){
+    var old = localStorage.getItem(name);
+    if(old === null) old = "";
+    localStorage.setItem(name, old + data);
+}
+</script>
+
+</body>
+</html>
+
+```
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,86 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" v-bind:class="currentTheme">
+    <!-- {{ currentTheme }} --> <!-- Show what is currently in localStorage-->
+    <h5 class="text-center title">(Light / Dark mode)</h5>
+    <div class="content-box">
+      <div class="theme-switcher-wrap">
+        <div
+          class="theme-switcher"
+          v-on:click="switchTheme"
+          v-bind:class="{'active': currentTheme === 'theme-dark'}"
+        >
+          <div class="switch-path">
+            <div class="switch-handle"></div>
+          </div>
+        </div>
+      </div>
+      <h3>Custom Dark Theme Toggler</h3>
+      <h5>(VUEJS. SCSS/CSS, Local Storage)</h5>
+      <p>
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
+        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
+        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+        voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
+        corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+        Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
+        quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
+        voluptas nulla pariatur?"
+      </p>
+      <p>-- WEMEET --</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      currentTheme: localStorage.getItem("theme-color"),
+    };
+  },
+  methods: {
+    switchTheme() {
+      // Logic
+      const storedTheme = localStorage.getItem("theme-color");
+      if (storedTheme === "theme-dark") {
+        localStorage.setItem("theme-color", "theme-light");
+        this.currentTheme = localStorage.getItem("theme-color");
+      } else {
+        localStorage.setItem("theme-color", "theme-dark");
+        this.currentTheme = localStorage.getItem("theme-color");
+      }
+    },
+  },
+};
 </script>
 
+
+
 <style>
+html {
+  height: 100%;
+}
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
+h5.title {
+  font-size: 22px;
+  margin: 0;
+  padding: 20px 0 0 0;
+}
+@import "main.css";
 </style>
